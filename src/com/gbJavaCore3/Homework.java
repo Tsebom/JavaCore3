6d90031dcccd9034119cd2c0189f7d3aa7795349
Tsebom <tsebom@yandex.ru>
2021-04-14 21:27:44 +0400

    start

---
+++ src/com/gbJavaCore3/Homework.java
@@ -0,0 +1,13 @@
+package com.gbJavaCore3;
+
+/**
+ * Homework
+ * @author - Nezhdanov Sergei
+ * @version 1.0
+ */
+public class Homework {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
